8d317f98cb95634990997dfd8393a1f23ef6e377
Raphaël Léger <raphael@nodixo.com>
2019-10-17 13:40:54 +0200

    chore/pypi: add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="constants_set",
+    version="0.1",
+    description="Simple constants sets for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raphael-leger/constants-set",
+    author="Raphaël Léger",
+    author_email="raphael+pypi@nodixo.com",
+    license="MIT",
+    packages=["constants_set"],
+    zip_safe=False,
+)
